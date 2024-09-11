1c269cd8cde3a713627bca54b99df892930c5933
Isabelle Fonseca <isabelle7536@gmail.com>
2024-09-10 23:00:40 -0300

    teste para ContaEspecial e ContaPoupanca

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'models/ContaEspecial.php';
+require_once 'models/ContaPoupanca.php';
+
+use app\models\ContaEspecial;
+use app\models\ContaPoupanca;
+
+// Teste ContaEspecial
+$contaEspecial = new ContaEspecial("Maria Silva", "1234", 5000.00, 1000.00);
+echo $contaEspecial->depositar(200.00) . "<br>";
+echo $contaEspecial->sacar(100.00) . "<br>";
+
+// Teste ContaPoupanca
+$contaPoupanca = new ContaPoupanca("João Oliveira", "5678", 2000.00, "01/01");
+
